b1e885ff05e25914fac4b54c61c1d02c1c115e02
Inhishonor <inhishonor@protonmail.com>
2025-09-03 17:55:42 -0700

    Twelve: Fix spacing around play buttons
    Change-Id: I2007dc81a5199beb3a085992ae7bc1e2e959e8cb

---
+++ app/src/main/java/org/lineageos/twelve/ui/layoutbehaviors/NowPlayingBottomSheetBehavior.kt
@@ -0,0 +1,8 @@
+package org.lineageos.twelve.ui.layoutbehaviors
+
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+
+class NowPlayingBottomSheetBehavior<V : View>() : BottomSheetBehavior<V>() {
+
+}
